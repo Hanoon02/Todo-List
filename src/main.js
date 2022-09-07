ae066c5fcc00a3f0b5a919110f095e5a089f5f2e
Hanoon02 <ahmedhanoon02@gmail.com>
2022-09-07 16:02:44 +0530

    Created Lists

--- src/main.js
+++ src/main.js
@@ -2,13 +2,20 @@ const addBtn = document.getElementById('add-task');
 const main = document.getElementById('tasks');
 const clear = document.getElementById('clear');
 const allTasks = document.getElementById('all-tasks');
-const view = document.getElementById('view');
+// const view = document.getElementById('view');
 const form = document.getElementById('form');
 
 var taskList = [];
 var taskCount = 0;
 
+clear.addEventListener('click', ()=>{
+    taskList = [];
+    taskCount = 0;
+    allTasks.innerHTML = '';
+})
 function addTask(){
+    const tempDiv = document.createElement('div');
+
     const newInput = document.createElement('input');
     newInput.classList.add('input');
     newInput.setAttribute('type', 'text');
@@ -18,16 +25,18 @@ function addTask(){
     newBtn.classList.add('btn');
     newBtn.textContent = 'Add';
     
-    form.appendChild(newInput);
-    form.appendChild(newBtn);
+    tempDiv.appendChild(newInput);
+    tempDiv.appendChild(newBtn);
+    form.appendChild(tempDiv); 
     
     const newItem = document.createElement('li');
     newItem.classList.add('item');
 
     newBtn.addEventListener('click', ()=>{
-        // allTasks.appendChild(newItem);
+        allTasks.appendChild(newItem);
         newItem.textContent = newInput.value;
         taskList.push(newItem.textContent);
+        tempDiv.innerHTML='';
     })
 }
 
@@ -35,6 +44,6 @@ addBtn.addEventListener('click', ()=>{
     taskCount++;
     addTask();})
 
-view.addEventListener('click', ()=>{
-    console.log(taskList);
-})
+// view.addEventListener('click', ()=>{
+//     console.log(taskList);
+// })
